c4ea1860579d8a2cbfea6df77ee5b753b7b13080
aaronrbg <aaronrbg@aaronrbg.com>
2018-12-13 21:52:06 -0800

    init and test projects

---
+++ test/App.js
@@ -0,0 +1,57 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import CacheImage from './components/CacheImage';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+  'Double tap R on your keyboard to reload,\n' +
+  'Shake or press menu button for dev menu',
+});
+
+const RNFS = require('react-native-fs');
+
+
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    console.log(RNFS.CachesDirectoryPath);
+    return (
+      <View style={styles.container}>
+        {/* <CacheImage /> */}
+        <CacheImage uri='http://localhost:8080/cat.jpg' style='img'/>
+        <Text style={styles.welcome}>${RNFS.CachesDirectoryPath}</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  }
+});
